93b741f0a0321a91651a4b85593c8c3edb4723a3
airermakova <56444081+airermakova@users.noreply.github.com>
2022-10-16 21:53:56 +0200

    Add files via upload

--- startweb.py
+++ startweb.py
@@ -5,7 +5,8 @@ import mechanicalsoup
 import sys
 import os
 
-phrases=""
+testmodelpath=os.getcwd()+"/testmodel"
+
 
 app = Flask(__name__)
 @app.route('/')
@@ -16,10 +17,44 @@ def home():
 def gettrainingset():
     return render_template('gettrainingset.html')
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in "pt"
 
-@app.route('/getchecknn/')
+@app.route('/getchecknn/', methods=['GET','POST'])
 def getchecknn():
-    return render_template('checknn.html')
+    path=""
+    output=""
+    args = []
+    if request.method == "POST":
+        print("POST")
+        #phrases number area
+        inputN = request.form.get('inputN')
+        num = request.form['inputN']
+        inputN = request.form.get('inputF')
+        st = request.form['inputF']
+        if num == None or num=="":
+             num = "0"
+        if st == None or st=="":
+             st = "0"
+        print(num + "..." + st)
+        #file uploading area
+        file = request.files['file']
+        if file.filename == '':
+            output = 'No model file selected'
+        elif allowed_file(file.filename):
+            file.save(os.path.join(testmodelpath,file.filename))
+            args.append(os.path.join(testmodelpath,"patents.txt"))
+            args.append(st)
+            args.append(num)
+            args.append(os.path.join(testmodelpath,file.filename))
+            command = "C:/Users/airer/AppData/Local/Programs/Python/Python36/python.exe FlaiNNTestMultiThreading.py " + " ".join(args)
+            os.system(command)
+            with open('NNStatistics.txt', 'r') as file:
+                output = file.read()
+        else:
+            output="uploaded file has to have .pt extention"            
+    return render_template('getchecknn.html', outputP=output)
 
 
 @app.route('/getusers/', methods=['GET','POST'])
@@ -31,7 +66,6 @@ def getusers():
         path = request.form['inputP']
         print("POST "+str(path))
         if len(str(path))>1:
-            phrases = path
             myString = str(path).strip('\n\t')
             arg = myString.replace(" ", "_")
             print(arg)
